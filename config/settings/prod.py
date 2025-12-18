13550ab85f2a641aa17088bf80b67e413bd2b34b
Neeschal1 <neeschalpok04@gmail.com>
2025-12-18 22:07:34 +0545

    secured database details

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,11 +6,11 @@ ALLOWED_HOSTS = []
 # Default Database (Postgre SQL) : For production
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "Travelperk",
-        "USER": "neondb_owner",
-        "PASSWORD": "npg_Xi1jDaLlhtB5",
-        "HOST": "ep-mute-sunset-a13tzyhd-pooler.ap-southeast-1.aws.neon.tech",
-        "PORT": "5432",
+        "ENGINE": Config.ENGINE,
+        "NAME": Config.NAME,
+        "USER": Config.USER,
+        "PASSWORD": Config.PASSWORD,
+        "HOST": Config.HOST,
+        "PORT": Config.PORT,
     }
 }
--- env_config.py
+++ env_config.py
@@ -4,4 +4,15 @@ import os
 load_dotenv()
 
 class Config:
+    # Secret Key
     SECRET_KEY = os.getenv('SECRET_KEY')
+    
+    # Database Info
+    NAME = os.getenv('NAME')
+    USER = os.getenv('USER')
+    PASSWORD = os.getenv('PASSWORD')
+    HOST = os.getenv('HOST')
+    PORT = os.getenv('PORT')
+    ENGINE = os.getenv('ENGINE')
+    
+    
