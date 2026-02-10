d5b158f0fbfaef4d0efff72c8bbc052b8cff5836
Neeschal1 <neeschalpok04@gmail.com>
2026-02-10 22:14:00 +0545

    necessary project setup completed!

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -6,20 +6,20 @@ ALLOWED_HOST = []
 
 
 # Database used for Development (db.sqlite3) : For Development Process only
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": Config.ENGINE,
-        "NAME": Config.NAME,
-        "USER": Config.USER,
-        "PASSWORD": Config.PASSWORD,
-        "HOST": Config.HOST,
-        "PORT": Config.PORT,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": Config.ENGINE,
+#         "NAME": Config.NAME,
+#         "USER": Config.USER,
+#         "PASSWORD": Config.PASSWORD,
+#         "HOST": Config.HOST,
+#         "PORT": Config.PORT,
+#     }
+# }
