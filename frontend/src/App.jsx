5659d0bddf356ab47260bf8b781d72cd86797598
Neeschal1 <neeschalpok04@gmail.com>
2026-02-11 21:38:28 +0545

    tailwind setup completed!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,26 +8,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex h-screen w-full items-center justify-center'>
+        <h1 className='font-medium text-amber-400'>Hi, this is travelperk</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
