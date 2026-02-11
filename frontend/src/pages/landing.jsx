f32507cf3afefb91d843a05fa5e2db1cd91d44cc
Neeschal1 <neeschalpok04@gmail.com>
2026-02-11 23:38:36 +0545

    simple navbar created

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../constants/navbar";
 
 const Landing = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex items-center justify-center">
-      <button
-        className="bg-black text-white"
-        onClick={() => {
-          navigate("/login");
-        }}
-      >
-        Login
-      </button>
-      <button
-        className="bg-black text-white"
-        onClick={() => {
-          navigate("/signup");
-        }}
-      >
-        Signup
-      </button>
-      <br />
-      <h1 className="text-2xl text-black">Hello Pulu 🔥</h1>
+    <div className="flex items-center justify-center w-full">
+      <Navbar />
     </div>
   );
 };
